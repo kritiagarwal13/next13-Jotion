647ce70c279a94fc6a763d6868fecb1f5b3da28b
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2024-01-11 13:57:09 +0000

    folder setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-emerald-500 text-white">Delete</Button>
+    </div>
+  );
+}
