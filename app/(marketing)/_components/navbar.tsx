cfb05aed865dfb3d7a6b91e688e64193a864ef28
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2024-01-11 16:20:44 +0000

    Landing page

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import { Logo } from "./logo";
+import { ModeToggle } from "@/components/mode-toggle";
+
+export const Navbar = () => {
+    const scrolled = useScrollTop();
+    return (
+        <div className={cn("z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex item-center w-full p-6", 
+        scrolled && "border-b shadow-sm")
+        }>
+            <Logo />
+            <div className="md:ml-auto md:justify-end justify-between w-fill flex items-center gap-x-2">
+                <ModeToggle />
+            </div>
+        </div>
+    )
+}
