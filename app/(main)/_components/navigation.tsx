f38b1cbbaa3b74bae87f4e43f2a3f5db1de95ee9
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2024-01-16 00:35:17 +0000

    Search functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -13,6 +13,7 @@ import {
   PopoverTrigger,
   PopoverContent,
 } from "@/components/ui/popover"
+import { useSearch } from "@/hooks/use-search";
 
 import { UserItem } from "./user-item";
 import { Item } from "./item";
@@ -21,6 +22,7 @@ import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -141,7 +143,7 @@ export const Navigation = () => {
           label="Search"
           icon={Search}
           isSearch
-          onClick={() => {}}
+          onClick={search.onOpen}
           />
           <Item
           label="Settings"
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,36 +2,33 @@
 import { redirect } from "next/navigation";
 
 import { Spinner } from "@/components/spinner";
+import { SearchCommand } from "@/components/search-command";
 import { useConvexAuth } from "convex/react";
 
 import { Navigation } from "./_components/navigation";
 
-const MainLayout = ({
-    children
-}: {
-    children: React.ReactNode;
-}) => {
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
-    const { isAuthenticated, isLoading } = useConvexAuth();
+  if (isLoading) {
+    <div className="h-full flex items-center justify-center">
+      <Spinner size="lg" />
+    </div>;
+  }
 
-    if (isLoading) {
-        <div className="h-full flex items-center justify-center">
-            <Spinner size="lg" />
-        </div>
-    }
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
 
-    if (!isAuthenticated) {
-        return redirect("/")
-    }
+  return (
+    <div className="h-full flex dark:bg-[#1f1f1f]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">
+        <SearchCommand />
+        {children}
+      </main>
+    </div>
+  );
+};
 
-    return ( 
-        <div className="h-full flex dark:bg-[#1f1f1f]">
-            <Navigation />
-            <main className="flex-1 h-full overflow-y-auto">
-            {children}
-            </main>
-        </div>
-     );
-}
- 
 export default MainLayout;
