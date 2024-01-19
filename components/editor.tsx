cc9b7d5e98e2f4f97e562b8f7321e3b0548aab51
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2024-01-19 01:24:24 +0000

    removes PartialBlock[]

--- components/editor.tsx
+++ components/editor.tsx
@@ -9,7 +9,7 @@ import {
   BlockNoteView,
   useBlockNote
 } from "@blocknote/react";
-import "@blocknote/core/style.css";
+import "@blocknote/react/style.css";
 
 import { useEdgeStore } from "@/lib/edgestore";
 
@@ -39,7 +39,7 @@ const Editor = ({
     editable,
     initialContent: 
       initialContent 
-      ? JSON.parse(initialContent) as PartialBlock[]
+      ? JSON.parse(initialContent)
       : undefined,
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
